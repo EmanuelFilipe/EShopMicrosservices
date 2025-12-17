50284eb8abbd98cf6a876fa0a5a82fa34d55672a
Filipe Silva <cloudwalstorn@gmail.com>
2025-12-16 21:09:05 -0300

    creating Category and Product Entities

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/Handler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    // representa dados necessarios para criar um produto
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+
+    // represebta objeto de retorno do objeto de resposta
+    public record CreateProductResult(Guid Id);
+
+    internal class Handler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
