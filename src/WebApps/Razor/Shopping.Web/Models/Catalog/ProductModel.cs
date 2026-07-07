0c242f113c0da9119ba4b31742451389c30946c4
EmanuelFilipe <emanuel_filipe@yahoo.com.br>
2026-07-07 07:34:29 -0300

    section 20 - commiting initial files of WebApp Razor project

---
+++ src/WebApps/Razor/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Web.Models.Catalog;
+
+public class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public List<string> Category { get; set; } = [];
+    public string Description { get; set; } = string.Empty;
+    public string ImageFile { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
+
+public record GetProductsResponse(IEnumerable<ProductModel> Products);
+public record GetProductByCategoryResponse(IEnumerable<ProductModel> Products);
+public record GetProductByIdResponse(ProductModel Product);
